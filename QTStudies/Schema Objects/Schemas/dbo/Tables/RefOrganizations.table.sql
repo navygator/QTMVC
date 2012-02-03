80591e64c1bad17baef2133d19aa8354c7b9a53c
Alexey Nureev <navygator@myopera.com>
2012-02-03 07:47:39 +0400

    Cleanup

--- QTStudies/Schema Objects/Schemas/dbo/Tables/RefOrganizations.table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[RefOrganizations]
-(
-	Id uniqueidentifier NOT NULL,
-	TeacherId uniqueidentifier NOT NULL,
-	Name nvarchar(150) NOT NULL, 
-	INN int NOT NULL
-)
--- QTStudies/Schema Objects/Schemas/dbo/Tables/RefTeachers.table.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[RefTeachers]
-(
-	Id uniqueidentifier NOT NULL,
-	FIO nvarchar(150) NOT NULL, 
-	EMail nvarchar(50)
-)
